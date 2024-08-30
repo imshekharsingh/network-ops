5f8b40d7bf7a22035574c5a74666dd0f6508673b
imshekharsingh <shekhar.singh20@outlook.com>
2024-08-30 14:45:06 +0530

    Dig Script: Added comments for clarity

--- dig-script/dig_script.py
+++ dig-script/dig_script.py
@@ -13,6 +13,7 @@ def write_host_file(domain_name, value):
     headers = ['Hostname', 'Dig Result']
     data = [[domain_name, value],]
 
+    # Check output file's existence and set headers
     file_path = "dig_result.csv"
     file_exists = os.path.isfile(file_path) and os.path.getsize(file_path) > 0
     
@@ -23,9 +24,13 @@ def write_host_file(domain_name, value):
         file.writerows(data)
 
 def dig_domain_name(domain_name,dns_server):
+    # Create a dns resolver instance and set nameserver
     resolver = dns.resolver.Resolver(configure=False)
     resolver.nameservers = [dns_server]
+
+    # DNS lookup operation below
     try:
+        # DNS lookup for domain's CNAME record
         response = resolver.resolve(domain_name, 'CNAME')
         for cname in response:
             print(f"CNAME record for {domain_name} is: {cname}")
@@ -34,6 +39,7 @@ def dig_domain_name(domain_name,dns_server):
     except dns.resolver.NoAnswer:
         print(f"No CNAME record found for {domain_name}")
         try:
+            # If no CNAME found, DNS lookup for domain's A record
             response = resolver.resolve(domain_name, 'A')
             for ip in response:
                 print(f"A record for {domain_name} is: {ip}")
@@ -41,16 +47,29 @@ def dig_domain_name(domain_name,dns_server):
             banner()
         except dns.resolver.NoAnswer:
             print(f"No A record found for {domain_name}")
+            try:
+                # If no CNAME found, DNS lookup for domain's A record
+                response = resolver.resolve(domain_name, 'AAAA')
+                for ip in response:
+                    print(f"AAAA record for {domain_name} is: {ip}")
+                    write_host_file(domain_name, ip)
+                banner()
+            except dns.resolver.NoAnswer:
+                print(f"No A record found for {domain_name}")
     except dns.resolver.NXDOMAIN:
         print(f"Domain {domain_name} does not exist!")
 
 def host_list(dns_server):
+    # Read hostnames list from input file
     with open('dig_hosts.txt', 'r') as file:
         for domain_name in file:
+            # From domain list avoid using any non-printable characters
             domain_name = ''.join(char for char in domain_name if char in string.printable and char not in string.whitespace)
+            
             dig_domain_name(domain_name,dns_server)
 
 def main():
+    # Define Nameserver to used for dig operation
     dns_server = sys.argv[1]
     host_list(dns_server)
 
